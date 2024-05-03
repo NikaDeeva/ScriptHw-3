666ae216c151ae6c35b59b9f959e6ffc4947f0d0
NikaDeeva <136634291+NikaDeeva@users.noreply.github.com>
2024-05-03 19:28:58 +0200

    ok

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ console.log(result);
 let email = 'nika159@gmail.com';
 console.log(email.includes('@'));
 console.log(email.length);
+
+//3
+const my = 'My';
+const name = 'name';
+const is = 'is';
+const fullName = my + ' ' + name + ' ' + is + ' ' + 'Viktor';
+console.log(fullName);
+
+//4
+const userName = 'Nika';
+const payment = 450;
+alert(`Thank you, ${userName}. For payment ${payment} dollars.`)
